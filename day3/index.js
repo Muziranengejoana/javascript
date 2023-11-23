fa50b9a20badbe1549ac2efa8ca74952775c88b8
joana <muziranengejoana@gmail.com>
2023-11-23 06:40:12 -0800

    boolean

---
+++ day3/index.js
@@ -0,0 +1,9 @@
+//booleans in js
+const age =10;
+console.log(Boolean(age));
+const sum =5;
+console.log(Boolean(1));
+const sub =1;
+console.log(boolean(0));
+const prod =6;
+console.log(boolean(-5));

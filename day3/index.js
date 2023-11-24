501a99a68eddae5116111706c4054ac964dc305f
joana <muziranengejoana@gmail.com>
2023-11-23 22:47:05 -0800

    boolean

--- day3/index.js
+++ day3/index.js
@@ -4,6 +4,6 @@ console.log(Boolean(age));
 const sum =5;
 console.log(Boolean(1));
 const sub =1;
-console.log(boolean(0));
+console.log(Boolean(0));
 const prod =6;
-console.log(boolean(-5));
+console.log(Boolean(-5));
